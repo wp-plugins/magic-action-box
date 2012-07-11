3c0bc6895f98690d6b566db69e692790e82a7b21
pogidude <pogidude@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-11 09:24:38 +0000

    tagging version 2.8.2
    git-svn-id: https://plugins.svn.wordpress.org/magic-action-box/tags/2.8.2@570527 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
-=== Magic Action Box ===
+=== Magic Action Box Pro===
 Contributors: prosulum, pogidude
 Developer: Prosulum
 Tags: opt in, call to action, aweber, email, email marketing, form, mailing list, marketing, newsletter, webform, mailchimp, constant contact
 Requires at least: 3.1
-Tested up to: 3.3.1
-Stable tag: 2.7.2
+Tested up to: 3.4.1
+Stable tag: 2.8.2
 
 Magic Action Box is an easy to use but powerful lead generation plugin.
 
@@ -19,7 +19,7 @@ Some features:
 * Make your own action box styles
 * Or, pick a style from one of the pre-configured designs
 
-[Upgrade to Pro Version](http://www.magicactionbox.com/)
+[Upgrade to Pro Version](http://www.magicactionbox.com/?pk_campaign=LITE&pk_kwd=pluginPage1)
 
 Some features available in Pro version:
 
@@ -28,7 +28,7 @@ Some features available in Pro version:
 * More pre-configured styles
 * Conversion Tips and Advice
 * Create and upload your own buttons
-* [And more...](http://www.magicactionbox.com/features/?aff=7)
+* [And more...](http://www.magicactionbox.com/features/?pk_campaign=LITE&pk_kwd=pluginPage2)
 
 == Installation ==
 
@@ -41,15 +41,19 @@ Some features available in Pro version:
 
 = My autoresponder is not on the list. Can I use it with the plugin? =
 
-Yes you can. As long as your autoresponder lets you create html forms that you can copy and paste, then you are all good. Watch this [video tutorial](http://www.magicactionbox.com/how-use-magic-action-box-with-any-email-marketing-service/) to learn more.
+Yes you can. As long as your autoresponder lets you create html forms that you can copy and paste, then you are all good. Watch this [video tutorial](http://www.magicactionbox.com/how-use-magic-action-box-with-any-email-marketing-service/?pk_campaign=LITE&pk_kwd=faq) to learn more.
 
 = Can I place an action box on my website's sidebar? =
 
-Maybe on later updates. But, that is not the main reason this plugin was created. The primary purpose of magic action box is for you to follow two very effective marketing strategies. You can read all about this tip [here](http://www.magicactionbox.com/magic-action-box-training-1-the-key-to-making-more-sales-and-version-2-0-sneak-peak/).
+Maybe on later updates. But, that is not the main reason this plugin was created. The primary purpose of magic action box is for you to follow two very effective marketing strategies. You can read all about this tip [here](http://www.magicactionbox.com/magic-action-box-training-1-the-key-to-making-more-sales-and-version-2-0-sneak-peak/?pk_campaign=LITE&pk_kwd=faq).
 
 = My action box doesn't seem to have any styles to it. What's wrong? =
 
 Make sure that your uploads directory wp-content/uploads/ is writeable (it usually is).
+
+= My action box styles are gone. What did your update do? =
+
+If you just upgrade from any version below 2.8, then this is expected due to the big improvements in the code. We're sorry for the scare but don't worry, just follow the steps [in this guide](http://www.magicactionbox.com/fixing-action-box-styles-v2-8-upgrade/?pk_campaign=LITE&pk_kwd=faq) and you should get back your converting users in no time.
 
 = I just recently updated the plugin and the action box looks weird. =
 
@@ -58,7 +62,6 @@ Try clearing the plugin cache using the *Clear Cache* button found in WP Admin -
 = The action box is showing up after some other social sharing icons placed by another plugin. But I want it to show up right after the blog post =
 
 Try checking the *Reorder post content filter priorities* checkbox found in WP Admin -> Magic Action Box Settings -> Main Settings. Please understand that this may not solve the problem and may conflict with other plugins too. Just uncheck it if it does.
-
 
 == Screenshots ==
 
@@ -69,8 +72,34 @@ Try checking the *Reorder post content filter priorities* checkbox found in WP A
 5. Action Box Settings Screen
 
 == Changelog ==
+= 2.8.2 =
+* added core improvements.
+* reorganized main settings page to have the same layout as pro
+
+= 2.8.1 =
+* Forgot to add unversioned files
+
+= 2.8 =
+* Made action box styles global
+* Added Share Box action box type
+* Renamed Design Settings menu to Styles & Buttons
+* Fixed PHP notices
+* Fixed action box styling issues on preconfigured styles
+* Added id parameter to shortcode
+* Added mab_get_actionbox() template tag function
+
 = 2.7.2 =
 * Fixed style not working for Sales Box type
+
+= 2.7.1 =
+* Fixed PHP notices
+
+= 2.7 =
+* Added new style "Purity"
+* Added new style "Purity Alternate"
+* Added new style "Lead"
+* Added new style "The Grey"
+* Added autoupdate functionality"
 
 = 2.6.2 =
 * Improved UI copy on Opt-In box settings screen.
@@ -124,10 +153,26 @@ Try checking the *Reorder post content filter priorities* checkbox found in WP A
 * Original Version.
 
 == Upgrade Notice ==
+= 2.8.1 =
+Forgot to add unversioned files
+
+= 2.8 =
+Lots of improvements
+
 = 2.7.2 =
 Fixed style not working for Sales Box type
 
-= 2.6.1 = Fix minor issue with visited links being overriden by plugin's stylesheet
+= 2.7.1 =
+Fix PHP notices
+
+= 2.7 =
+This version now has autoupdate functionality
 
 = 2.6 =
 This version fixes the issue where stylesheet is not loading when User Settings in the style setting is selected.
+
+= 1.0 =
+Upgrade notices describe the reason a user should upgrade.  No more than 300 characters.
+
+= 0.5 =
+This version fixes a security related bug.  Upgrade immediately.
--- views/settings/design.php
+++ views/settings/design.php
@@ -1,16 +1,21 @@
 <div class="themes-php">
 	<div class="wrap">
 		<?php screen_icon('edit-comments'); ?>
-		<h2 id="mab-settings-header">Designs</h2>
+<h2 class="nav-tab-wrapper">
+	<a id="mab-designs-tab" href="#mab-styles" class="nav-tab">Styles</a>
+	<a id="mab-buttons-tab" href="#mab-buttons" class="nav-tab">Buttons</a>
+</h2>
 <!-- END HEADER -->
 
 <?php 
 ## MESSAGES
 if( isset( $_GET['deleted'] ) && 'true' == $_GET['deleted'] ):
 ?>
-<div id="mab-settings-deleted-message" class="updated fade"><p><strong><?php esc_html_e( 'Item Deleted', 'mab' ); ?></strong></p></div>
+	<div id="mab-settings-deleted-message" class="updated fade"><p><strong><?php esc_html_e( 'Item Deleted', 'mab' ); ?></strong></p></div>
+<?php elseif( isset( $_GET['duplicated'] ) && 'true' == $_GET['duplicated'] ) : ?>
+	<div id="mab-settings-duplicated-message" class="updated fade"><p><strong><?php esc_html_e( 'Style Duplicated', 'mab' ); ?></strong></p></div>
 <?php elseif( isset( $_GET['mab-preconfigured-buttons'] ) && 'true' == $_GET['mab-preconfigured-buttons'] ): ?>
-<div id="mab-settings-preconfigured-message" class="updated fade"><p><strong><?php esc_html_e( 'Preconfigured Buttons Created', 'mab' ); ?></strong></p></div>
+	<div id="mab-settings-preconfigured-message" class="updated fade"><p><strong><?php esc_html_e( 'Preconfigured Buttons Created', 'mab' ); ?></strong></p></div>
 <?php endif;
 
 ##END MESSAGES
@@ -18,42 +23,82 @@ if( isset( $_GET['deleted'] ) && 'true' == $_GET['deleted'] ):
 
 <?php
 $buttons = $data['buttons'];
+$styles = $data['styles'];
 ?>
 
-<h3><?php _e('CSS3 Buttons', 'mab' ); ?> <a href="<?php echo esc_url(admin_url('admin.php?page=mab-button-settings')); ?>" class="button button-primary"><?php _e('Create New', 'mab' ); ?></a> <a href="<?php echo esc_url( wp_nonce_url( add_query_arg( array( 'page' => 'mab-design', 'mab-create-preconfigured' => 'true' ), admin_url('admin.php') ), 'mab-create-preconfigured' ) ); ?>" class="button button-secondary"><?php _e('Create Preconfigured Buttons', 'mab' ); ?></a></h3>
+<div id="mab-styles" class="group">
+	<h3><?php _e('Action Box Styles', 'mab' ); ?> <a href="<?php echo esc_url(admin_url('admin.php?page=mab-style-settings')); ?>" class="button button-primary"><?php _e('Create New Style', 'mab' ); ?></a></h3>
 
-<table class="widefat">
-	<thead>
-		<tr>
-			<th scope="col"><?php _e('Title','mab' ); ?></th>
-			<th scope="col"><?php _e('Last Saved', 'mab' ); ?></th>
-			<th scope="col"><?php _e('Example Button', 'mab' ); ?></th>
-			<th scope="col"><?php _e('Actions', 'mab' ); ?></th>
-		</tr>
-	</thead>
-	<tfoot>
-		<tr>
-			<th scope="col"><?php _e('Title','mab' ); ?></th>
-			<th scope="col"><?php _e('Last Saved', 'mab' ); ?></th>
-			<th scope="col"><?php _e('Example Button', 'mab' ); ?></th>
-			<th scope="col"><?php _e('Actions', 'mab' ); ?></th>
-		</tr>
-	</tfoot>
-	<tbody>
-		<?php //var_dump( $buttons ); 
-		foreach( $buttons as $key => $button ):
-		?>
-		<tr>
-			<td><?php echo esc_html( $button['title'] ); ?></td>
-			<td><?php echo esc_html(date( 'F j, Y \a\t g:iA', $button['timesaved'] ) ); ?></td>
-			<td><a href="#" onclick="return false;" style="display:block; float: left; margin: 5px 0;" class="mab-button-<?php echo $key; ?>"><?php echo esc_html($button['title']); ?></a></td>
-			<td><a href="<?php echo esc_url( add_query_arg( array( 'page' => 'mab-button-settings', 'mab-button-id' => $key), admin_url('admin.php') ) ); ?>"><?php _e('Edit','mab'); ?></a>
-			<?php //TODO: Add "Duplicate" link ?>
-			| <a href="<?php echo esc_url( wp_nonce_url( add_query_arg( array( 'page' => 'mab-design', 'mab-button-id' => $key, 'mab-delete-button' => 'true'), admin_url('admin.php') ), 'mab-delete-button') ); ?>"><?php _e('Delete','mab' ); ?></a></td>
-		</tr>
-		<?php endforeach; ?>
-	</tbody>
-</table>
+	<table class="widefat">
+		<thead>
+			<tr>
+				<th scope="col"><?php _e('Name','mab' ); ?></th>
+				<th scope="col"><?php _e('Last Saved', 'mab' ); ?></th>
+				<th scope="col"><?php _e('Actions', 'mab' ); ?></th>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<th scope="col"><?php _e('Name','mab' ); ?></th>
+				<th scope="col"><?php _e('Last Saved', 'mab' ); ?></th>
+				<th scope="col"><?php _e('Actions', 'mab' ); ?></th>
+			</tr>
+		</tfoot>
+		<tbody>
+			<?php //var_dump( $buttons ); 
+			foreach( $styles as $style_key => $style ):
+			?>
+			<tr>
+				<td><?php echo esc_html( $style['title'] ); ?></td>
+				<td><?php echo esc_html(date( 'F j, Y \a\t g:iA', $style['timesaved'] ) ); ?></td>
+				<td>
+					<a href="<?php echo esc_url( add_query_arg( array( 'page' => 'mab-style-settings', 'mab-style-key' => $style_key), admin_url('admin.php') ) ); ?>"><?php _e('Edit','mab'); ?></a>
+				
+					| <a href="<?php echo esc_url( wp_nonce_url( add_query_arg( array( 'page'=> 'mab-design', 'mab-style-key' => $style_key, 'mab-duplicate-style' => 'true' ), admin_url( 'admin.php' ) ), 'mab-duplicate-style' ) ); ?>"><?php _e('Duplicate', 'mab' ); ?></a>
+				
+					| <a href="<?php echo esc_url( wp_nonce_url( add_query_arg( array( 'page' => 'mab-design', 'mab-style-key' => $style_key, 'mab-delete-style' => 'true'), admin_url('admin.php') ), 'mab-delete-style') ); ?>"><?php _e('Delete','mab' ); ?></a></td>
+			</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+</div><!-- #mab-designs -->
+
+<div id="mab-buttons" class="group">
+	<h3><?php _e('CSS3 Buttons', 'mab' ); ?> <a href="<?php echo esc_url(admin_url('admin.php?page=mab-button-settings')); ?>" class="button button-primary"><?php _e('Create New', 'mab' ); ?></a> <a href="<?php echo esc_url( wp_nonce_url( add_query_arg( array( 'page' => 'mab-design', 'mab-create-preconfigured' => 'true' ), admin_url('admin.php') ), 'mab-create-preconfigured' ) ); ?>" class="button button-secondary"><?php _e('Create Preconfigured Buttons', 'mab' ); ?></a></h3>
+	
+	<table class="widefat">
+		<thead>
+			<tr>
+				<th scope="col"><?php _e('Title','mab' ); ?></th>
+				<th scope="col"><?php _e('Last Saved', 'mab' ); ?></th>
+				<th scope="col"><?php _e('Example Button', 'mab' ); ?></th>
+				<th scope="col"><?php _e('Actions', 'mab' ); ?></th>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<th scope="col"><?php _e('Title','mab' ); ?></th>
+				<th scope="col"><?php _e('Last Saved', 'mab' ); ?></th>
+				<th scope="col"><?php _e('Example Button', 'mab' ); ?></th>
+				<th scope="col"><?php _e('Actions', 'mab' ); ?></th>
+			</tr>
+		</tfoot>
+		<tbody>
+			<?php //var_dump( $buttons ); 
+			foreach( $buttons as $key => $button ):
+			?>
+			<tr>
+				<td><?php echo esc_html( $button['title'] ); ?></td>
+				<td><?php echo esc_html(date( 'F j, Y \a\t g:iA', $button['timesaved'] ) ); ?></td>
+				<td><a href="#" onclick="return false;" style="display:block; float: left; margin: 5px 0;" class="mab-button-<?php echo $key; ?>"><?php echo esc_html($button['title']); ?></a></td>
+				<td><a href="<?php echo esc_url( add_query_arg( array( 'page' => 'mab-button-settings', 'mab-button-id' => $key), admin_url('admin.php') ) ); ?>"><?php _e('Edit','mab'); ?></a>
+				<?php //TODO: Add "Duplicate" link ?>
+				| <a href="<?php echo esc_url( wp_nonce_url( add_query_arg( array( 'page' => 'mab-design', 'mab-button-id' => $key, 'mab-delete-button' => 'true'), admin_url('admin.php') ), 'mab-delete-button') ); ?>"><?php _e('Delete','mab' ); ?></a></td>
+			</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+</div><!-- #mab-buttons -->
 
 <!-- FOOTER -->
 	</div><!-- .wrap -->
