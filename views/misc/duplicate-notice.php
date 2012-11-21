a6ac639dd2c7866385fc3d954b6b6b7f4074b1f4
pogidude <pogidude@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-21 04:21:18 +0000

    tagged v2.9
    git-svn-id: https://plugins.svn.wordpress.org/magic-action-box/tags/2.9@628014 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/misc/duplicate-notice.php
@@ -0,0 +1,3 @@
+<div class="updated fade" id="mab-duplicated-message">
+	<p><?php printf( __('A duplicate of this action box has been created. you can <a href="%s" target="_blank">edit it here</a>.', MAB_DOMAIN ), get_edit_post_link( $data['duplicate-id'] ) ); ?></p>
+</div>
